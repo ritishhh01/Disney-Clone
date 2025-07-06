7e226349b3d25ce9e43fe67d01dac5b94c94c17a
ritishhh01 <ritish992@gmail.com>
2025-07-06 18:53:19 +0530

    added categories and firebase db integration

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,17 +5,67 @@ import Recommends from './Recommends';
 import NewDisney from './NewDisney';
 import Originals from './Originals';
 import Trending from './Trending';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import db from '../firebase';
+import { setMovies } from '../features/movie/movieSlice';
+import { selectUserName } from '../features/user/userSlice';
+import { collection, onSnapshot } from 'firebase/firestore';
 
 const Home = () => {
-    return <Container>
-        <ImgSlider/>
-        <Viewers/>
-        <Recommends/>
-        <NewDisney/>
-        <Originals/>
-        <Trending/>
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, 'movies'), (snapshot) => {
+      let recommends = [];
+      let newDisneys = [];
+      let originals = [];
+      let trending = [];
+
+      snapshot.docs.forEach((doc) => {
+
+        switch (doc.data().type) {
+          case 'recommend':
+            recommends = [...recommends, { id: doc.id, ...doc.data() }];
+            break;
+          case 'new':
+            newDisneys = [...newDisneys, { id: doc.id, ...doc.data() }];
+            break;
+          case 'original':
+            originals = [...originals, { id: doc.id, ...doc.data() }];
+            break;
+          case 'trending':
+            trending = [...recommends, { id: doc.id, ...doc.data() }];
+            break;
+          default:
+            break;
+        }
+      });
+
+      dispatch(setMovies({
+        recommend: recommends,
+        newDisney: newDisneys,
+        original: originals,
+        trending: trending
+      }));
+    });
+
+    return () => unsubscribe();
+  }, [userName, dispatch]);
+
+  return (
+    <Container>
+      <ImgSlider />
+      <Viewers />
+      <Recommends />
+      <NewDisney />
+      <Originals />
+      <Trending />
     </Container>
+  );
 };
+
 
 const Container = styled.main`
     position: relative;
--- src/components/NewDisney.js
+++ src/components/NewDisney.js
@@ -1,31 +1,25 @@
 import styled from "styled-components";
 import { Link } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { selectNewDisney } from "../features/movie/movieSlice";
 
 const NewDisney = () => {
+    const movies = useSelector(selectNewDisney);
+
     return (
     <Container>
         <h4>New Disney</h4>
         <Content>
-            <Wrap>
-                <Link to='/'>
-                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/49B92C046117E89BC9243A68EE277A3B30D551D4599F23C10BF0B8C1E90AEFB6/scale?width=1440&aspectRatio=1.78&format=jpeg" alt=''/>
-                </Link>
-            </Wrap>
-            <Wrap>
-                <Link to='/'>
-                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/49B92C046117E89BC9243A68EE277A3B30D551D4599F23C10BF0B8C1E90AEFB6/scale?width=1440&aspectRatio=1.78&format=jpeg" alt=''/>
-                </Link>
-            </Wrap>
-            <Wrap>
-                <Link to='/'>
-                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/49B92C046117E89BC9243A68EE277A3B30D551D4599F23C10BF0B8C1E90AEFB6/scale?width=1440&aspectRatio=1.78&format=jpeg" alt=''/>
-                </Link>
-            </Wrap>
-            <Wrap>
-                <Link to='/'>
-                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/49B92C046117E89BC9243A68EE277A3B30D551D4599F23C10BF0B8C1E90AEFB6/scale?width=1440&aspectRatio=1.78&format=jpeg" alt=''/>
-                </Link>
-            </Wrap>
+            {
+                movies && movies.map((movie, key)=>{
+                    return <Wrap key={key}>
+                        {movie.id}
+                        <Link to={'/detail/' + movie.id}>
+                            <img src={movie.cardImg} alt={movie.title}/>
+                        </Link>
+                    </Wrap>
+                })
+            }
         </Content>
     </Container>
     );
